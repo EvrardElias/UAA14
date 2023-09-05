0a1f38b9f7ddc48bb6b6237e0a5889de571c0030
Evrard Elias <190353@CCM-Asty-Moulin.local>
2023-09-05 14:47:16 +0200

    Premier exercice classe

---
+++ maPremiereClasse/maPremiereClasse/Chien.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chien
+{
+    class Chien
+    {
+        private string _nom;
+        private uint _age;
+        private string _race;
+        private bool _enOrdreDeVaccin;
+        private bool _puce;
+        private string _genre;
+        private bool _sterilise;
+        private string _poils;
+        private string _paysOrigine;
+        private string _proprietaire;
+        private bool _dresse;
+
+        public Chien (string nom, uint age, string race,  bool enOrdreDeVaccin, bool puce, string genre, bool sterilise, string poils, string paysOrigine, string proprietaire, bool dresse)
+        {
+            _nom = nom;
+            _age = age;
+            _race = race;
+            _enOrdreDeVaccin = enOrdreDeVaccin;
+            _puce = puce;
+            _genre = genre;
+            _sterilise = sterilise;
+            _poils = poils;
+            _paysOrigine = paysOrigine;
+            _proprietaire = proprietaire;
+            _dresse = dresse;
+        }
+
+        public string AfficherCaracteristique ()
+        {
+            string chaine = "Nom : " + _nom + " - Age : " + _age + " - Race : " + _race + " - En ordre de vaccin : " + _enOrdreDeVaccin + " - Puce : " + _puce + " - Genre : " + _genre + " - Sterilise : " + _sterilise + " - Poils : " + _poils + " - Pays d'origine : " + _paysOrigine + " - Proprietaire : " + _proprietaire + " - Dresser : " + _dresse + ".";
+            return chaine;
+        }
+    }
+}
---
+++ maPremiereClasse/maPremiereClasse/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Chien 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("première méthode");
+            Chien chien1 = new Chien("Nelson", 10, "Golden Rotriever", false, false, "Male", false, "Doré", "Belgique", "Romane Grootaers", false);
+            Chien chien2 = new Chien("James", 7, "Cavalier King Charles", false, false, "Male", false, "Brun", "Belgique", "Romane Grootaers", false);
+            Chien chien3 = new Chien("Djo", 12, "Berger Allemand", false, false, "Male", false, "Brun", "Belgique", "Elias Evrard", false);
+
+            Console.WriteLine(chien1.AfficherCaracteristique());
+            Console.WriteLine(chien2.AfficherCaracteristique());
+            Console.WriteLine(chien3.AfficherCaracteristique());
+        }
+    }
+}
