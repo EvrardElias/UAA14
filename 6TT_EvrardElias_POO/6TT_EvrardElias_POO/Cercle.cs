a8da701ce789f7c86acaf2192f9c3043d217d072
Evrard Elias <190353@CCM-Asty-Moulin.local>
2023-09-19 14:39:10 +0200

    Act 2 POO exercice 1

---
+++ 6TT_EvrardElias_POO/6TT_EvrardElias_POO/Cercle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6TT_EvrardElias_POO
+{
+    class Cercle
+    {
+        private float _rayon;
+
+        public Cercle(float rayon)
+        {
+            _rayon = rayon;
+        }
+
+        public float Rayon
+        {
+            get { return _rayon; }
+            set { _rayon = value; }
+        }
+
+        public float CalculePerimetre()
+        {
+            float perimetre = (float)(2 * Math.PI * Rayon);
+            return perimetre;
+        }
+
+        public float CalculeAire()
+        {
+            float aire = (float)(Math.PI * (Rayon * Rayon));
+            return aire;
+        }
+
+        public string AfficherCalcul()
+        {
+            string chaine = "Le cercle de rayon " + Rayon + " a un périmètre de " + CalculePerimetre() + " et une aire de " + CalculeAire() + ".";
+            return chaine;
+        }
+    }
+}
---
+++ 6TT_EvrardElias_POO/6TT_EvrardElias_POO/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _6TT_EvrardElias_POO;
+
+Console.WriteLine("Bienvenue dans ce programme sur le cercle !");
+Console.WriteLine("Tapez un rayon pour votre cercle.");
+float rayon = float.Parse(Console.ReadLine());
+
+Cercle monCercle = new Cercle(rayon);
+Console.WriteLine(monCercle.AfficherCalcul());
