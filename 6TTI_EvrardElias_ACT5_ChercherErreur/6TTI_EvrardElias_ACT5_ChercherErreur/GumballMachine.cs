6c040730686bcf03e7644ca3be940f78bd469336
Evrard Elias <190353@CCM-Asty-Moulin.local>
2023-12-01 14:24:07 +0100

    POO ACT5 Chercher Erreur

---
+++ 6TTI_EvrardElias_ACT5_ChercherErreur/6TTI_EvrardElias_ACT5_ChercherErreur/GumballMachine.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6TTI_EvrardElias_ACT5_ChercherErreur
+{
+    class GumballMachine
+    {
+        private int _gumballs;
+        private int _price;
+
+        public int Price
+        {
+            get
+            {
+                return _price;
+            }
+        }
+
+        public GumballMachine(int gumballs, int price)
+        {
+            this._gumballs = gumballs;
+            _price = price;
+        }
+
+        public string DispenseOneGumball(int coinsInserted)
+        {
+                if (coinsInserted >= _price)
+                {
+                    _gumballs -= 1;
+                    return "Voici votre gomme !";
+                }
+                else
+                {
+                    return "Pas assez de pièces !";
+                }
+        }
+    }
+}
