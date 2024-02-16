fd76bde48071aec1fc493d265d03a5b228ea32b7
Evrard Elias <190353@CCM-Asty-Moulin.local>
2024-02-16 12:36:04 +0100

    Heritage

---
+++ BDD/6TTI_CRUD_Evrard/6TTI_CRUD_Evrard/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Data;
+
+namespace _6TTI_CRUD_Evrard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] donnees = new string[8];
+            DataSet immoBiens;
+            methodemodele monModele = new methodemodele();
+            Console.WriteLine("Liste des utilisateurs");
+            Console.WriteLine("----------------------");
+            if (monModele.ChercherBiens(out immoBiens))
+            {
+                string listeBien = monModele.AfficheDataBiens(immoBiens);
+                Console.WriteLine(listeBien);
+            }
+            else
+            {
+                Console.WriteLine("Pb lors de la lecture des données");
+            }
+            Console.WriteLine("Ajout d'un bien :");
+            Console.WriteLine("------------------------");
+            Console.WriteLine("Nom ? ");
+            donnees[0] = Console.ReadLine();
+            Console.WriteLine("Taille ? ");
+            donnees[1] = Console.ReadLine();
+            Console.WriteLine("Prix ? ");
+            donnees[2] = Console.ReadLine();
+            Console.WriteLine("Ville ? ");
+            donnees[3] = Console.ReadLine();
+            Console.WriteLine("User Id ? ");
+            donnees[4] = Console.ReadLine();
+            Console.WriteLine("Description ? ");
+            donnees[5] = Console.ReadLine();
+            Console.WriteLine("Chambres ? ");
+            donnees[6] = Console.ReadLine();
+            Console.WriteLine("Image bien ? ");
+            donnees[7] = Console.ReadLine();
+
+            if (monModele.AjouterBien(donnees))
+            {
+                Console.WriteLine("Bien ajouté !");
+            }
+            else
+            {
+                Console.WriteLine("Problème lors de l'ajout !");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BDD/6TTI_CRUD_Evrard/6TTI_CRUD_Evrard/methodemodele.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Diagnostics;
+using Google.Protobuf.WellKnownTypes;
+using System.Data.Common;
+
+namespace _6TTI_CRUD_Evrard
+{
+    internal class methodemodele
+    {
+        static string DefinirCheminBD() // détermine la chaîne de connexion
+        {
+            return "server=10.10.51.98;database=immo;port=3306;User Id=elias;password=root";
+        }
+
+        public bool ChercherBiens(out DataSet infos)
+        {
+            bool ok = false;
+            MySqlConnection maConnection = new MySqlConnection(DefinirCheminBD());
+            string query = "";
+            try
+            {
+                maConnection.Open();
+
+                query = "Select * FROM biens";
+
+                MySqlDataAdapter da = new MySqlDataAdapter(query, maConnection);
+                infos = new DataSet();
+                da.Fill(infos, "infoBiens");
+                if (infos.Tables[0].Rows.Count >= 1)
+                {
+                    ok = true;
+                }
+                maConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                throw;
+            }
+            return ok;
+        }
+
+        public bool AjouterBien(string[] donnees)
+        {
+            bool ok = false;
+            MySqlConnection maConnection = new MySqlConnection(DefinirCheminBD());
+        }
+
+        public string AfficheDataUsers(DataSet donnees)
+        {
+            string infos = "";
+            for (int i = 0; i < donnees.Tables[0].Rows.Count; i++)
+            {
+                infos += donnees.Tables[0].Rows[i]["nomBien"].ToString() + " | " +
+                         donnees.Tables[0].Rows[i]["tailleBien"].ToString() + " | " +
+                         donnees.Tables[0].Rows[i]["prixBien"].ToString() + " | " +
+                         donnees.Tables[0].Rows[i]["villeBien"].ToString() + " | " +
+                         donnees.Tables[0].Rows[i]["userIdBien"].ToString() + " | " +
+                         donnees.Tables[0].Rows[i]["descriptionBien"].ToString() + " | " +
+                         donnees.Tables[0].Rows[i]["chambreBien"].ToString() + " | " +
+                         donnees.Tables[0].Rows[i]["imageBien"].ToString() + " | " + "\n";
+            }
+            return infos;
+        }
+    }
+}
